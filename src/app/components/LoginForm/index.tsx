7709f8423ba0b201da784618dfcff37e64791f23
ruugii_31 <roger.barrero.sorribas@gmail.com>
2025-05-20 21:32:28 +0200

    feat: refactor LoginForm to use reusable Input and Button components

--- src/app/components/LoginForm/index.tsx
+++ src/app/components/LoginForm/index.tsx
@@ -1,6 +1,8 @@
-'use client'
+"use client";
 
 import loginUser from "@/app/api/login/login";
+import Button from "@/app/ux/Button";
+import Input from "@/app/ux/Input";
 import { useState } from "react";
 
 export default function LoginForm() {
@@ -8,43 +10,41 @@ export default function LoginForm() {
   const [password, setPassword] = useState("");
   return (
     <form className="flex flex-col space-y-4">
-        <input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
-          placeholder="Correo electrónico"
-          className="border border-gray-300 p-2 rounded"
-        />
-        <input
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          placeholder="Contraseña"
-          className="border border-gray-300 p-2 rounded"
-        />
-        <button
-          type="submit"
-          className="bg-vollio-accent text-white p-2 rounded hover:bg-vollio-dark transition duration-200"
-          onClick={(e) => {
-            e.preventDefault();
-            console.log("Logging in user:", { email, password });
-            loginUser({
-              email,
-              password
+      <Input
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        type="email"
+        placeholder="Correo electrónico"
+      />
+      <Input
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        type="password"
+        placeholder="Contraseña"
+      />
+
+      <Button
+        type="submit"
+        onClick={(e) => {
+          e.preventDefault();
+          console.log("Logging in user:", { email, password });
+          loginUser({
+            email,
+            password,
+          })
+            .then((res) => {
+              const token = res.token;
+              sessionStorage.setItem("token", token);
+              window.location.href = "/";
             })
-              .then((res) => {
-                const token = res.token;
-                sessionStorage.setItem("token", token);
-                window.location.href = "/";
-              })
-              .catch((error) => {
-                console.error("Error in login:", error);
-                alert("Error al iniciar sesión");
-              });
-          } }
-        >
-          Iniciar Sesión
-        </button>
-      </form>
-  )
+            .catch((error) => {
+              console.error("Error in login:", error);
+              alert("Error al iniciar sesión");
+            });
+        }}
+      >
+        Iniciar Sesión
+      </Button>
+    </form>
+  );
 }
