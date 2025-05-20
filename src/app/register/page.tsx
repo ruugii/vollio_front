e4c07b675298260bbdae9fd9a476140503ad7fac
ruugii_31 <roger.barrero.sorribas@gmail.com>
2025-05-20 21:38:48 +0200

    feat: replace h1 tag with H1_component in RegisterPage for consistency

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,12 @@
 import RegisterForm from "../components/RegisterForm";
+import H1_component from "../Text/H1_component";
 
 export default function RegisterPage() {
   return (
     <main className="container mx-auto h-full px-4 py-8">
-      <h1 className="text-4xl font-bold mb-4">Registrarse</h1>
+      <H1_component className="mb-4">
+        Registrarse
+      </H1_component>
       <RegisterForm />
     </main>
   )
