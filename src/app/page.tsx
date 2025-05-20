22441b92d9b4130ce99d67dc78de228b7838eafb
ruugii_31 <roger.barrero.sorribas@gmail.com>
2025-05-20 18:50:30 +0200

    feat: replace HTML elements with custom Text, H1, and H2 components for consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
+import Text from "./Text/Text";
+import H1_component from "./Text/H1_component";
+import H2_component from "./Text/H2_component";
+
 export default function Home() {
   return (
     <main className="container mx-auto h-full px-4 py-8">
-      <h1 className="text-4xl font-bold mb-4">Bienvenido a <span className="text-vollio-accent">vollio</span></h1>
-      <p className="text-lg">
+      <H1_component className="mb-4">
+        Bienvenido a <span className="text-vollio-900">vollio</span>
+      </H1_component>
+      <Text>
         La plataforma de gestion de equipos y partidos de voley playa amateur.
-      </p>
-      <h2 className="text-2xl font-bold mt-8 mb-4">La comunidad de vollio</h2>
-      <p className="text-lg">
-      </p>
+      </Text>
+      <H2_component className="mt-8 mb-4">
+        La comunidad de vollio
+      </H2_component>
     </main>
   );
 }
