3af41a77f6a0ab7adb36047418232610b4fe238a
ruugii_31 <roger.barrero.sorribas@gmail.com>
2025-05-20 18:54:47 +0200

    feat: update Header background and text color for improved visibility

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -3,7 +3,7 @@ import PrivateHeader from "@/app/ux/PrivateHeader";
 
 export default function Header() {
   return (
-    <header className="bg-vollio-white text-vollio-dark py-4 hidden md:block shadow-md">
+    <header className="bg-vollio-50 text-vollio-950 py-4 hidden md:block shadow-md">
       <nav className="container mx-auto px-4 flex flex-wrap items-center justify-between">
         <div className="text-2xl font-bold">Vollio</div>
         <ul className="flex space-x-6 md:space-x-8">
