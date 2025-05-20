9226f322238ee515d36eb84dcf409dfff0a447d8
ruugii_31 <roger.barrero.sorribas@gmail.com>
2025-05-20 18:54:39 +0200

    feat: update Footer background and text color for improved visibility

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -2,7 +2,7 @@ import MenuItem from "@/app/ux/MenuItem";
 
 export default function Footer() {
   return (
-    <footer className="bg-vollio-white text-vollio-dark py-4 hidden md:block shadow-md">
+    <footer className="bg-vollio-50 text-vollio-950 py-4 hidden md:block shadow-md">
       <nav className="container mx-auto px-4 flex flex-wrap items-center justify-between">
         <ul className="flex space-x-6 md:space-x-8">
           <MenuItem href="/" className="text-lg">
