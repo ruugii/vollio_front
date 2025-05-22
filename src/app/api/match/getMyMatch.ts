d1c2c597b173aa2135ef024fcc22ff7f87d7aac8
ruugii_31 <roger.barrero.sorribas@gmail.com>
2025-05-22 22:36:55 +0200

    feat: implement getMyMatch function for fetching user match data

---
+++ src/app/api/match/getMyMatch.ts
@@ -0,0 +1,29 @@
+import { BASE_API_URL } from "@/app/data";
+
+export default async function getMyMatch(token: string) {
+  return fetch(`${BASE_API_URL}/match/get/me`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        console.error("Error creating match:", res.statusText);
+        alert("Error al crear el partido");
+      }
+    })
+    .then((data) => {
+      console.log("Response from createMatch:", data);
+      if (data.match) {
+        return data;
+      }
+    })
+    .catch((error) => {
+      console.error("Error in create match:", error);
+      alert("Error al crear el partido");
+    });
+}
