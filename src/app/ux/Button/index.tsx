80b7574318bb58e6f124863f57fdd946c67eed41
ruugii_31 <roger.barrero.sorribas@gmail.com>
2025-05-22 23:36:06 +0200

    fix: prevent pointer events on disabled button

--- src/app/ux/Button/index.tsx
+++ src/app/ux/Button/index.tsx
@@ -5,17 +5,20 @@ export default function Button({
   type,
   className,
   onClick,
+  disabled,
 }: {
   children: ReactNode;
   type?: "button" | "submit" | "reset";
   className?: string;
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
 }) {
   return (
     <button
       type={type}
-      className={`${className} bg-vollio-300 text-vollio-950 p-2 rounded hover:bg-vollio-400 disabled:bg-vollio-200 transition duration-200 hover:cursor-pointer`}
+      className={`${className} bg-vollio-300 text-vollio-950 p-2 rounded hover:bg-vollio-400 disabled:bg-vollio-200 transition duration-200 hover:cursor-pointer disabled:pointer-events-none`}
       onClick={onClick}
+      disabled={disabled}
     >
       {children}
     </button>
