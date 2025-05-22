852b307d08039e010fc298b04a73381bb276ab2e
ruugii_31 <roger.barrero.sorribas@gmail.com>
2025-05-22 19:12:43 +0200

    feat: update createMatch function to handle date as string or Date and improve error handling

--- src/app/api/match/createMatch.ts
+++ src/app/api/match/createMatch.ts
@@ -1,9 +1,14 @@
 export default async function createMatch(
   token: string,
-  { location, date }: { location: string; date: Date }
+  { location, date }: { location: string; date: Date | string }
 ) {
   try {
-    return await fetch(`http://localhost:4000/api/v1/match/create`, {
+    const dateString = typeof date === "string" ? date : date.toISOString(); // Asegura UTC en formato ISO
+
+    console.log("Creating match with token:", token);
+    console.log("Match details:", { location, date: dateString });
+
+    const response = await fetch(`http://localhost:4000/api/v1/match/create`, {
       method: "POST",
       headers: {
         Authorization: `Bearer ${token}`,
@@ -11,21 +16,16 @@ export default async function createMatch(
       },
       body: JSON.stringify({
         location,
-        date,
+        date: dateString,
       }),
-    })
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        } else {
-          console.error("Error creating match:", res.statusText);
-          alert("Error al crear el partido");
-        }
-      })
-      .catch((error) => {
-        console.error("Error in create match:", error);
-        alert("Error al crear el partido");
-      });
+    });
+
+    if (response.ok) {
+      return await response.json();
+    } else {
+      console.error("Error creating match:", response.statusText);
+      alert("Error al crear el partido");
+    }
   } catch (error) {
     console.error("Error creating match:", error);
     alert("Error al crear el partido");
