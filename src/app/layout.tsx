88b63f0d56c6e83884b33fbee11de4a7d90d62f9
ruugii_31 <roger.barrero.sorribas@gmail.com>
2025-05-23 12:04:43 +0200

    add meta adds

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,40 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '646613635026798');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <link
+            rel="preload"
+            as="image"
+            href="https://www.facebook.com/tr?id=646613635026798&ev=PageView&noscript=1"
+          />
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=646613635026798&ev=PageView&noscript=1"
+            alt="facebook pixel"
+          />
+        </noscript>
+      </head>
       <body
         className={`min-h-screen bg-vollio-white text-vollio-dark ${geistSans.variable} ${geistMono.variable} font-sans antialiased grid`}
       >
